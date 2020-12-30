28c471b5a9acd6364390ae3c72718f937d8d715e
WKH <206982362@qq.com>
2020-12-30 22:00:41 +0800

    new

--- spider.py
+++ spider.py
@@ -14,7 +14,7 @@ import sys
 
 #????
 # 伪造header
-headers2 = [
+headers = [
     {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"},
     {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0"},
